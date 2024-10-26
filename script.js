786c7d44cd00e65cc21c88e0d2c461708628b2f7
Rebecca D. <rdolmon08@gmail.com>
2024-10-26 15:33:53 -0500

    adding key-values js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
+const myFavoriteFootballTeam = {
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
+
+};
